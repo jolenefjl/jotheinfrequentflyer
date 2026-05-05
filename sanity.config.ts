e343703ee050c3702f435166d04abbece52ce7cf
Jolene Foo-Hodne <jolene@strategytools.io>
2026-05-05 23:51:49 +0200

    Restore native Sanity publish actions

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "@/sanity/schemaTypes";
-import { publishDocumentAction } from "@/sanity/documentActions/publish-document";
 
 export default defineConfig({
   name: "default",
@@ -13,21 +12,5 @@ export default defineConfig({
   plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
-  },
-  document: {
-    actions: (previousActions, context) => {
-      const schemaType = context.schemaType as string | { name?: string };
-      const schemaTypeName = typeof schemaType === "string" ? schemaType : schemaType.name;
-
-      if (schemaTypeName) {
-        const alreadyHasCustomPublish = previousActions.some(
-          (action) => action.displayName === publishDocumentAction.displayName,
-        );
-        const withoutDefaultPublish = previousActions.filter((action) => action.action !== "publish");
-        return alreadyHasCustomPublish ? previousActions : [publishDocumentAction, ...withoutDefaultPublish];
-      }
-
-      return previousActions;
-    },
   },
 });
